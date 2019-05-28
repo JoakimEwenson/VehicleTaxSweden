0accffdd4999af2801bb734d9d2b2c03191ade82
Joakim Ewenson <34868402+JoakimEwenson@users.noreply.github.com>
2019-05-28 14:21:48 +0200

    Kotlin WIP

--- VehicleTaxSweden.cs
+++ VehicleTaxSweden.cs
@@ -9,142 +9,139 @@
 
 using System;
 
-class VehicleTaxSweden
+namespace VehicleTaxSweden
 {
-    // Set up default value for later use
-    int vehicleTax = 0;
-
-    // Set up default components
-    public const int basicFee = 360;
-    public const int carbonComponent = 22;
-    public const int carbonComponentEco = 11;
-    public const int carbonComponentThreshold = 111;
-
-    // Set up bonus/malus components
-    public const int malusComponentFeeLow = 82;
-    public const int malusComponentFeeHigh = 107;
-    public const int malusComponentLevelLow = 95;
-    public const int malusComponentLevelHigh = 140;
-    public const int malusModelYear = 2018;
-
-    // Set up specific diesel components
-    public const int dieselComponentFeeLow = 250;
-    public const int dieselComponentFeeHigh = 500;
-    public const int dieselComponentYear = 2008;
-    public const double dieselFuelFactor = 13.52;
-    public const double dieselMultFactor = 2.37;
-
-    // Main calculation
-    public int VehicleTaxCalculation(int carbonOutput, int modelYear, bool isBonusMalus, bool isDiesel, bool isEcoFuel)
+    public class VehicleTax
     {
-        // Initialize by setting vehicle tax to basic, minimum fee
-        vehicleTax += basicFee;
+        // Set up default value for later use
+        public int vehicleTaxResult = 0;
 
-        // Check if vehicle is under the new bonus/malus system
-        if (modelYear >= malusModelYear)
+        // Set up default components
+        public const int basicFee = 360;
+        public const int carbonComponent = 22;
+        public const int carbonComponentEco = 11;
+        public const int carbonComponentThreshold = 111;
+
+        // Set up bonus/malus components
+        public const int malusComponentFeeLow = 82;
+        public const int malusComponentFeeHigh = 107;
+        public const int malusComponentLevelLow = 95;
+        public const int malusComponentLevelHigh = 140;
+        public const int malusModelYear = 2018;
+
+        // Set up specific diesel components
+        public const int dieselComponentFeeLow = 250;
+        public const int dieselComponentFeeHigh = 500;
+        public const int dieselComponentYear = 2008;
+        public const double dieselFuelFactor = 13.52;
+        public const double dieselMultFactor = 2.37;
+
+        // Main calculation
+        public int VehicleTaxCalculation(int carbonOutput, int modelYear, bool isBonusMalus, bool isDiesel, bool isEcoFuel)
         {
-            if (isBonusMalus)
+            // Initialize by setting vehicle tax to basic, minimum fee
+            vehicleTaxResult += basicFee;
+
+            // Check if vehicle is under the new bonus/malus system
+            if (modelYear >= malusModelYear)
             {
-                // Get the difference between vehicle carbon output and maximum allowed output
-                if (carbonOutput > malusComponentLevelHigh)
+                if (isBonusMalus)
                 {
-                    // Initialize variables
-                    int malusDiffHigh, malusDiffLow, malusTaxHigh, malusTaxLow, carbonDiff, carbonFee;
-
-                    // Get the difference between output and highest level
-                    malusDiffHigh = carbonOutput - malusComponentLevelHigh;
-                    malusDiffLow = (carbonOutput - malusDiffHigh) - malusComponentLevelLow;
-
-                    // Calculate the fee
-                    malusTaxHigh = malusDiffHigh * malusComponentFeeHigh;
-                    malusTaxLow = malusDiffLow * malusComponentFeeLow;
-
-                    vehicleTax += malusTaxHigh;
-                    vehicleTax += malusTaxLow;
-                }
-                else
-                {
-                    vehicleTax += (carbonOutput - malusComponentLevelLow) * malusComponentFeeLow;
-                }
-            }
-            // Check if eco fuel is used and if so, calculate with lower fee
-            if (isEcoFuel)
-            {
-                vehicleTax += (carbonOutput - carbonComponentThreshold) * carbonComponentEco;
-            }
-            // Check if diesel is used and if so, add additional taxes
-            if (isDiesel)
-            {
-                // Initialize variables
-                int dieselComponentFee;
-
-                if (!isBonusMalus)
-                {
-                    // Check carbon output above threshold
-                    if (carbonOutput > carbonComponentThreshold)
+                    // Get the difference between vehicle carbon output and maximum allowed output
+                    if (carbonOutput > malusComponentLevelHigh)
                     {
-                        vehicleTax += (carbonOutput - carbonComponentThreshold) * carbonComponent;
+                        // Initialize variables
+                        int malusDiffHigh, malusDiffLow, malusTaxHigh, malusTaxLow;
+
+                        // Get the difference between output and highest level
+                        malusDiffHigh = carbonOutput - malusComponentLevelHigh;
+                        malusDiffLow = (carbonOutput - malusDiffHigh) - malusComponentLevelLow;
+
+                        // Calculate the fee
+                        malusTaxHigh = malusDiffHigh * malusComponentFeeHigh;
+                        malusTaxLow = malusDiffLow * malusComponentFeeLow;
+
+                        vehicleTaxResult += malusTaxHigh;
+                        vehicleTaxResult += malusTaxLow;
+                    }
+                    else
+                    {
+                        vehicleTaxResult += (carbonOutput - malusComponentLevelLow) * malusComponentFeeLow;
                     }
                 }
-                // Check if vehicle is newer than current level
-                if (modelYear >= dieselComponentYear)
-                {
-                    dieselComponentFee = dieselComponentFeeLow;
-                }
-                else
-                {
-                    dieselComponentFee  = dieselComponentFeeHigh;
-                }
-
-                vehicleTax += (carbonOutput * dieselFuelFactor) + dieselComponentFee;
-            }
-        }
-
-        // If vehicle not under the new system, calculate the tax according to the older model
-        else
-        {
-            // Get the difference between vehicle carbon output and allowed threshold
-            int carbonDiff = carbonOutput - carbonComponentThreshold;
-            if (carbonDiff > 0)
-            {
-                // Check if vehicle use eco fuel and if so, use a lower fee
+                // Check if eco fuel is used and if so, calculate with lower fee
                 if (isEcoFuel)
                 {
-                    vehicleTax += carbonDiff * carbonComponentEco;
+                    vehicleTaxResult += (carbonOutput - carbonComponentThreshold) * carbonComponentEco;
                 }
-                else
+                // Check if diesel is used and if so, add additional taxes
+                if (isDiesel)
                 {
-                    vehicleTax += carbonDiff * carbonComponent;
+                    // Initialize variables
+                    int dieselComponentFee;
+
+                    if (!isBonusMalus)
+                    {
+                        // Check carbon output above threshold
+                        if (carbonOutput > carbonComponentThreshold)
+                        {
+                            vehicleTaxResult += (carbonOutput - carbonComponentThreshold) * carbonComponent;
+                        }
+                    }
+                    // Check if vehicle is newer than current level
+                    if (modelYear >= dieselComponentYear)
+                    {
+                        dieselComponentFee = dieselComponentFeeLow;
+                    }
+                    else
+                    {
+                        dieselComponentFee = dieselComponentFeeHigh;
+                    }
+
+                    vehicleTaxResult += (int)(carbonOutput * dieselFuelFactor) + dieselComponentFee;
                 }
             }
-            if (isDiesel)
+
+            // If vehicle not under the new system, calculate the tax according to the older model
+            else
             {
-                int dieselComponentFee;
-                if (modelYear >= dieselComponentYear)
+                // Get the difference between vehicle carbon output and allowed threshold
+                int carbonDiff = carbonOutput - carbonComponentThreshold;
+                if (carbonDiff > 0)
                 {
-                    dieselComponentFee = dieselComponentFeeLow;
+                    // Check if vehicle use eco fuel and if so, use a lower fee
+                    if (isEcoFuel)
+                    {
+                        vehicleTaxResult += carbonDiff * carbonComponentEco;
+                    }
+                    else
+                    {
+                        vehicleTaxResult += carbonDiff * carbonComponent;
+                    }
                 }
-                else
+                if (isDiesel)
                 {
-                    dieselComponentFee = dieselComponentFeeHigh;
+                    int dieselComponentFee;
+                    if (modelYear >= dieselComponentYear)
+                    {
+                        dieselComponentFee = dieselComponentFeeLow;
+                    }
+                    else
+                    {
+                        dieselComponentFee = dieselComponentFeeHigh;
+                    }
+                    vehicleTaxResult = (int)(vehicleTaxResult * dieselMultFactor) + dieselComponentFee;
                 }
-                vehicleTax = (vehicleTax * dieselMultFactor) + dieselComponentFee;
             }
-        }
 
-        // Check if calculated tax is below the minimum, basic feee
-        if (vehicleTax < basicFee)
-        {
-            vehicleTax = basicFee;
-        }
+            // Check if calculated tax is below the minimum, basic fee
+            if (vehicleTaxResult < basicFee)
+            {
+                vehicleTaxResult = basicFee;
+            }
 
-        // Output vehicle tax from the calculation
-        return vehicleTax;
-    }
-    
-    // Main method
-    static void Main()
-    {
-        // TODO: Add some stuff here...
+            // Output vehicle tax from the calculation
+            return vehicleTaxResult;
+        }
     }
 }
--- VehicleTaxSweden.kt
+++ VehicleTaxSweden.kt
@@ -38,5 +38,108 @@ val dieselMultFactor: Double = 2.37
 // Set up the calculations
 fun VehicleTaxCalculation(carbonOutput: Int, modelYear: Int, isBonusMalus: Boolean, isDiesel: Boolean, isEcoFuel: Boolean): Int
 {
+    // Initialize by setting the vehicle tax to basic fee
+    vehicleTax = basicFee
 
+    // Check if vehicle is under the new bonus/malus system
+    if (modelYear >= malusModelYear)
+    {
+        // Check if bonus/malus-flag is set to true
+        if (isBonusMalus)
+        {
+            // Get the difference between vehicle carbon output and allowed threshold
+            if (carbonOutput > malusComponentHigh)
+            {
+                var malusDiffHigh: Int, malusDiffLow: Int, malusTaxHigh: Int, malusTaxLow: Int
+
+                // Get the difference between output and highest level
+                malusDiffHigh = carbonOutput - malusComponentHigh
+                malusDiffLow = (carbonOutput - malusDiffHigh) - malusComponentLevelLow
+
+                // Calculate the fee
+                malusTaxHigh = malusDiffHigh * malusComponentHigh
+                malusTaxLow = malusDiffLow * malusComponentLow
+
+                vehicleTax += malusTaxHigh
+                vehicleTax += malusTaxLow
+            }
+            else
+            {
+                vehicleTax += (carbonOutput - malusComponentLevelLow) * malusComponentLow
+            }
+        }
+        // Check if eco fuel is used and if so, calculate with lower fee
+        if (isEcoFuel)
+        {
+            vehicleTax += (carbonOutput - carbonComponentLevel) * carbonComponentEco
+        }
+        // Check if diesel is used and if so, add additional taxes
+        if (isDiesel)
+        {
+            // Initialize variables
+            var dieselComponentFee: Int
+
+            if (!isBonusMalus)
+            {
+                // Check carbon output above threshold
+                if (carbonOutput > carbonComponentLevel)
+                {
+                    vehicleTax += (carbonOutput - carbonComponentLevel) * carbonComponent
+                }
+            }
+            // Check if vehicle is newer than current threshold
+            if (modelYear >= dieselComponentModelYear)
+            {
+                dieselComponentFee = dieselComponentLow
+            }
+            else {
+                dieselComponentFee = dieselComponentHigh
+            }
+
+            vehicleTax += (carbonOutput * dieselFuelFactor) + dieselComponentFee
+        }
+    }
+
+    // If vehicle is not under the new system, calculate the tax according to older model
+    else
+    {
+        // Get the difference between vehicle carbon output and allowed threshold
+        var carbonDiff: Int = carbonOutput - carbonComponentLevel
+
+        if (carbonDiff > 0)
+        {
+            // Check if vehicle use eco fuel and if so, use a lower fee
+            if (isEcoFuel)
+            {
+                vehicleTax += carbonDiff * carbonComponentEco
+            }
+            else
+            {
+                vehicleTax += carbonDiff * carbonComponent
+            }
+        }
+        if (isDiesel)
+        {
+            var dieselComponentFee: Int
+
+            if (modelYear >= dieselComponentModelYear)
+            {
+                dieselComponentFee = dieselComponentLow
+            }
+            else
+            {
+                dieselComponentFee = dieselComponentHigh
+            }
+
+            vehicleTax = (vehicleTax * dieselMultFactor) + dieselComponentFee
+        }
+    }
+
+    // Check if calculated tax is below the minimum fee
+    if (vehicleTax < basicFee)
+    {
+        vehicleTax = basicFee
+    }
+
+    return vehicleTax
 }
